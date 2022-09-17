3da8b852a1967374d725c4f0638a2880e0a1ca8d
mieukie <91030763+mieukie@users.noreply.github.com>
2022-09-17 12:30:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function ignite1() {
+    document.getElementById("flame1").style.visibility="visible"
+    console.log("red signal")
+}
+function ignite2() {
+    document.getElementById("flame2").style.visibility="visible"
+}
+function ignite3() {
+    document.getElementById("flame3").style.visibility="visible"
+}
+function ignite4() {
+    document.getElementById("flame4").style.visibility="visible"
+}
+function ignite5() {
+    document.getElementById("flame5").style.visibility="visible"
+}
+function ignite6() {
+    document.getElementById("flame6").style.visibility="visible"
+}
+function ignite7() {
+    document.getElementById("flame7").style.visibility="visible"
+}
+
+function unignite1() {
+    document.getElementById("flame1").style.visibility="visible"
+    console.log("red signal")
+}
+function unignite2() {
+    document.getElementById("flame2").style.visibility="visible"
+}
+function unignite3() {
+    document.getElementById("flame3").style.visibility="visible"
+}
+function unignite4() {
+    document.getElementById("flame4").style.visibility="visible"
+}
+function unignite5() {
+    document.getElementById("flame5").style.visibility="visible"
+}
+function unignite6() {
+    document.getElementById("flame6").style.visibility="visible"
+}
+function unignite7() {
+    document.getElementById("flame7").style.visibility="visible"
+}
